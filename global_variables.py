f81e702839934c97d94a209522dc8981d12ffa2b
Nelio <minecraftrabbit.g@gmail.com>
2022-07-12 19:43:04 +0200

    added some stuff

--- global_variables.py
+++ global_variables.py
@@ -1,6 +1,7 @@
 import discord
 
-token = 'Nzk2NzYwNzI5NzIxMzcyNjgy.Gl-dub.yEWEgZYbcAQZfl_ib0Mk8knA8Y_95_Jdvfzs9g'
+with open('.token', 'r') as f:
+    token = f.read()
 client = discord.Client()
 server_id = '961157523073802340'  # ID of the test server
 voice_channel_id = '961157523073802344'  # ID of the test voice channe
